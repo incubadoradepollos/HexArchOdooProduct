c83f92e936e917d11829a8b781819c3541ee5026
Incubadora de Pollos <incubadoradepollos@gmail.com>
2025-03-30 10:45:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hexarch_product_odoo",
-    version="0.1.1",
+    version="0.0.1",
     packages=find_packages(),
     author="Jose Manuel Herrera Saenz",
     author_email="incubadoradepollos@gmail.com",
